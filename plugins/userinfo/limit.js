4b2916cb160bfc2b43586fc04ce2a8d9881b9a8e
data-asepitgans <133214500+data-asepitgans@users.noreply.github.com>
2023-09-20 02:25:29 +0700

    Update limit.js

--- plugins/userinfo/limit.js
+++ plugins/userinfo/limit.js
@@ -8,7 +8,10 @@ exports.run = {
    }) => {
       let user = global.db.users.find(v => v.jid == m.sender)
       if (user.limit < 1) return client.reply(m.chat, `🚩 Your bot usage has reached the limit and will be reset at 00.00\n\nTo get more limits, upgrade to a premium plan send *${isPrefix}premium*`, m)
-      client.reply(m.chat, `🍟 Your limit : [ *${Func.formatNumber(user.limit)}* ]${!user.premium ? `\n\nTo get more limits, upgrade to a premium plan send *${isPrefix}premium*` : ''}`, m)
+      client.sendMessageModify(m.chat, `🍟 Your limit : [ *${Func.formatNumber(user.limit)}* ]${!user.premium ? `\n\nTo get more limits, upgrade to a premium plan send *${isPrefix}premium*` : ''}`, m, {
+        largeThumb: true,
+        thumbnail: 'https://telegra.ph/file/495bb295e2660fbe55fb0.jpg'
+      })
    },
    error: false
 }
