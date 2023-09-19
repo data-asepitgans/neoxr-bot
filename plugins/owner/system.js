861111ea9f09a534f0f2e76869f0cbeaeb939781
data-asepitgans <133214500+data-asepitgans@users.noreply.github.com>
2023-09-20 02:04:02 +0700

    Update system.js

--- plugins/owner/system.js
+++ plugins/owner/system.js
@@ -1,5 +1,5 @@
 exports.run = {
-   usage: ['autodownload', 'debug', 'groupmode', 'multiprefix', 'noprefix', 'online', 'self'],
+   usage: ['chatbot', 'autodownload', 'debug', 'groupmode', 'multiprefix', 'noprefix', 'online', 'self'],
    use: 'on / off',
    category: 'owner',
    async: async (m, {
